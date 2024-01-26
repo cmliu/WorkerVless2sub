495308cd3535a10af8924c3e852236276919a99e
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-01-27 00:59:52 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -253,7 +253,7 @@ addEventListener('fetch', event => {
 		  }
 	}
   
-	if (userAgent.includes('telegram') || userAgent.includes('twitter') || userAgent.includes('miaoko') || userAgent.includes('subconverter')) {
+	if (userAgent.includes('telegram') || userAgent.includes('twitter') || userAgent.includes('miaoko')) {
 		return new Response('Hello World!');
 	} else if (userAgent.includes('clash')) {
 		const subconverterUrl = `https://${subconverter}/sub?target=clash&url=${encodeURIComponent(request.url)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&new_name=true`;
