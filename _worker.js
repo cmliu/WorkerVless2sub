bad5e7dffa91acbeded83c3a6ddb6f2c59f8c82c
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-02-26 15:11:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,7 +16,7 @@ let addressescsv = [
 ];
 
 let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/edgetunnel/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 
 let link = '';
 let edgetunnel = 'ed';
