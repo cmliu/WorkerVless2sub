90dabbd6171b1c50c695520c082d1cdfe0bb3afb
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-04-02 00:28:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,27 +5,19 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'127.0.0.1:40000#WRAP',
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'127.0.0.1:40000#WRAP', 
 ];
 
 // 设置优选地址api接口
-let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+let addressesapi = [ 
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
-let addressesnotls = [
-	'www.visa.com.sg#官方优选域名',
-	'www.wto.org:8080#官方优选域名',
-	'www.who.int:8880#官方优选域名',
+let addressesnotls = [ 
 ];
 
 // 设置优选noTLS地址api接口
-let addressesnotlsapi = [
-	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt',
+let addressesnotlsapi = [ 
 ];
 
 let DLS = 1;//速度下限
