34838eec16866cb9b28f364cb38dd1ed24ed7a13
hurttttr <945399100@qq.com>
2024-07-31 11:57:25 +0800

    修改了_worker.js文件

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	'https://raw.githubusercontent.com/hurttttr/WorkerVless2sub/main/addressesapi.txt',, //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
 
