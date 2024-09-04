3729ffec3421b97450bb747fba01f93d0f91a770
xsll111 <hzq8710@gmail.com>
2024-09-05 02:02:40 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -5,8 +5,8 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'#icook.tw:2053#官方优选域名',
+	'#cloudflare.cfgo.cc#优选官方线路',
 ];
 
 // 设置优选地址api接口
