f6786086d345dc7c254dc93e35e9608e772e7204
ayive <zaylivey@gmail.com>
2024-11-16 16:26:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -32,7 +32,7 @@ let addressescsv = [
 	//'https://raw.githubusercontent.com/ayive/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
 ];
 
-let subconverter = "suburl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/refs/heads/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini"; //订阅转换配置文件
 let noTLS = 'false'; //改为 true , 将不做域名判断 始终返回noTLS节点
 let link;
